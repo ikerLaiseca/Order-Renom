82134de76d2a189435b049c459cc56dff8fdbc3f
ikerLaiseca <iklaise@gmail.com>
2015-05-09 11:47:54 +0200

    añadido filtro para hawaii five-0

--- renom.py
+++ renom.py
@@ -10,7 +10,7 @@ def renombrarYMover(origen,destino):
    print 'fecha ' + fecha
    for fn in os.listdir(origen):
       if fn[-4:] == ".jpg":
-         nombre=fn[0:-4:]
+        nombre=fn[0:-4:]
         #eliminamos espacios en blanco a izq y dcha
         nombre.strip()
         nn='' 
@@ -19,13 +19,37 @@ def renombrarYMover(origen,destino):
         os.rename(fn,nn)
         shutil.move(fn,destino)
         totalesFotos += 1
-      if fn[-4:] == ".avi":
+      if fn[-4:] == ".mp4":
+        print 'mp4'
+        #Hawaii.Five-0.2010.S03E10.HDTV.x264-LOL.[VTV].mp4
         nombre=fn[0:-4:]
         #eliminamos espacios en blanco a izq y dcha
-        nombre.strip()
-        nombre.lstrip('[]')
+        #nombre.strip()
+        #nombre.lstrip('[]')
+        #Hawaii.Five-0.2010.S03E10.HDTV.x264-LOL.
+        print 'nombre[:6] ' +nombre[:6]
+        if nombre[:6] == 'Hawaii':
+          print 'Hawaii'
+          print fn
+          rutaCarpeta = 'Hawaii Five-0/'
+          #S03
+          temporada = fn[19:22]
+          print 'Temporada: ' + temporada;
+          #E10
+          capitulo = fn[22:25]
+          print 'Capitulo ' + capitulo
+          nombre = 'Hawaii Five-0 ' + temporada +capitulo + '.mp4'
+          os.rename(fn,nombre)
+          directorio = os.path.join('series',temporada)
+          directorio = directorio.replace('\n', '')
+          print 'directorio: ' + directorio
+          if not os.path.isdir(directorio):
+            print 'not'
+            os.makedirs(directorio)
+            shutil.move(nombre, directorio)
+          else:
+            print 'yes'
         print 'nombre de la pelicula ' + nombre
-        os.rename(fn,nombre)
         totalesPeliculas += 1
    totales = 'Fotos movidas: ' + str(totalesFotos) + ' / Peliculas movidas: ' + str(totalesPeliculas) 
    return totales
